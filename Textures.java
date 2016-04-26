27cffc442cb1d8ce3e56979ccc1bac091485e662
allierowe4 <acr2785@uncw.edu>
2016-04-26 00:07:37 -0400

    Update Textures.java

--- Textures.java
+++ Textures.java
@@ -1,14 +1,25 @@
+	/**
+	 * @author Allison, Jack, Jordan
+	 * (Jack Created this Class)
+	 */ 
 import java.awt.image.BufferedImage;
-
+/**
+ * Textures class grabes images from sprites and adds them to the game
+ */ 
 public class Textures {
 	public BufferedImage playerF, playerB, playerL, playerR, bulletF, bulletB, bulletL, bulletR, zombieDub, zombieSing, zombieNone;
 	private SpriteSheet ss;
-	
+	/**
+	 * Textures puts the sprites onto the game class
+	 */ 
 	public Textures(Game game){
 		ss = new SpriteSheet(game.getSpriteSheet());
 		getTextures();
 	}
-	
+	/**
+	 * getTextures
+	 * grabes the specific images from spritesheet and assigns them names
+	 */ 
 	private void getTextures(){
 		playerF = ss.grabImage(2,  2, 32, 32);
 		playerB = ss.grabImage(1,  2, 32, 32);
