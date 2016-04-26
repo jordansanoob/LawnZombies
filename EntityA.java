6988b6fa8831bd4ba0df86d54f49f09fa5958f27
allierowe4 <acr2785@uncw.edu>
2016-04-25 23:37:27 -0400

    Update EntityA.java

--- EntityA.java
+++ EntityA.java
@@ -1,6 +1,13 @@
+/**
+ *@author Allison, Jack, Jordan 
+ * (Allison Created this class)
+ */ 
+
 import java.awt.Graphics;
 import java.awt.Rectangle;
-
+/**
+ *Interface Entity deals with the tick, render, getX, and getY instances
+ */ 
 public interface EntityA {
 
 	public void tick();
