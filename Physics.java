856ff4d30de3008e0114a3838918464ebd20492c
allierowe4 <acr2785@uncw.edu>
2016-04-25 23:58:48 -0400

    Update Physics.java

--- Physics.java
+++ Physics.java
@@ -1,7 +1,16 @@
+/**
+ * @author Allison, Jack, Jordan
+ * (We all created this Class)
+ */ 
 import java.util.LinkedList;
-
+/**
+ * Physics class
+ * gets bounds and adds the collisions
+ */ 
 public class Physics {
-	
+	/**
+	 * calculations behind the collison of the bullet to the zombie
+	 */ 
 	public static boolean Collision(EntityA enta, LinkedList<EntityB> entb){
 		for(int i = 0; i < entb.size(); i++){
 			if(enta.getBounds().intersects(entb.get(i).getBounds())){
@@ -11,7 +20,9 @@ public class Physics {
 	}
 	
 	return false;
-}
+}        /**
+	 * calculations behind the collison of the bullet to the zombie
+	 */ 
 	public static boolean Collision(EntityB entb, LinkedList<EntityA> enta){
 	for(int i = 0; i < enta.size(); i++){
 		if(entb.getBounds().intersects(enta.get(i).getBounds())){
