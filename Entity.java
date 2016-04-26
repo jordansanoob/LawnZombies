60c1c662c05c197d0d1fb4e5bbb6a89046234456
allierowe4 <acr2785@uncw.edu>
2016-04-25 23:36:32 -0400

    Update Entity.java

--- Entity.java
+++ Entity.java
@@ -1,7 +1,12 @@
-//package con.gam.src.main;
+/**
+ *@author Allison, Jack, Jordan 
+ * (Allison Created this class)
+ */ 
 
 import java.awt.Graphics;
-
+/**
+ *Interface Entity deals with the tick, render, getX, and getY instances
+ */ 
 public interface Entity {
 	public void tick();
 	public void render(Graphics g);
