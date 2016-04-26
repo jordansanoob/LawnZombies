f5bf238675a5f39c45b458ca1049e6f2ad0abcf3
allierowe4 <acr2785@uncw.edu>
2016-04-25 23:34:18 -0400

    Update Controller.java

--- Controller.java
+++ Controller.java
@@ -1,4 +1,4 @@
-	import java.awt.Graphics;
+import java.awt.Graphics;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -8,27 +8,34 @@ import java.util.Random;
 	 * Controller will use LinkedList to keep track of the zombies and bullets on screen.
 	 * In both its tick and draw method it will move the zombies and bullets accordingly and will eventually 
 	 * hold our collision method
-	 *  
+	 *(Created by Allison, Jack, and Jordan) 
 	 */
 	public class Controller{
-		private LinkedList<EntityA> ea = new LinkedList<EntityA>();
+		private LinkedList<EntityA> ea = new LinkedList<EntityA>(); 
 		private LinkedList<EntityB> eb = new LinkedList<EntityB>();
-		EntityA entA; 
+		EntityA entA; //Created in seporate Classes
 		EntityB entB;
 		private Textures tex;
-		Random r = new Random();
+		Random r = new Random(); //generate random for the zoombies getting added to the game class
 		private Game game;
-		
+		/**
+		 * sets the controller to deal with the textures and the game 
+		 */ 
 		public Controller(Textures tex, Game game){
 			this.tex = tex;
 			this.game = game;
 		}
+		/**
+		 *CreateZombie method counts the zombies nad creates them
+		 */ 
 		 public void createZombie(int eCount){
 			 for(int i = 0; i < eCount; i++){
 				 addEntity(new Zombie(r.nextInt(640), -10, tex, this, game));
 			 }
 		 }
-		
+		/**
+		 * tick method gets the size of the EntityA and EntitiyB
+		 */ 
 		public void tick(){
 			
 			//A Class
@@ -42,6 +49,9 @@ import java.util.Random;
 				entB.tick();
 			}
 		}
+		/**
+		 * Render method
+		 */ 
 		public void render(Graphics g){
 			//A Class
 			for(int i = 0; i<ea.size(); i++){
@@ -55,21 +65,39 @@ import java.util.Random;
 				entB.render(g);
 			}
 		}
+		/**
+		 * Adds the bullet
+		 */ 
 		public void addEntity(EntityA bullet){
 			ea.add(bullet);
 		}
+		/**
+		 *removes the zombie when collision occurs
+		 */ 
 		public void removeEntity(EntityA obj){
 			ea.remove(obj);
 		}
+		/**
+		 *adds the next zombie after it is removed
+		 */ 
 		public void addEntity(EntityB obj){
 			eb.add(obj);
 		}
+		/**
+		 *removes the zombies when collision occurs 
+		 */ 
 		public void removeEntity(EntityB obj){
 			eb.remove(obj);
 		}
+		/**
+		 *EntityA return method
+		 */ 
 		public LinkedList<EntityA> getEntityA(){
 			return ea;
 		}
+		/**
+		 *EntityB return method
+		 */ 
 		public LinkedList<EntityB> getEntityB(){
 			return eb;
 		}	
