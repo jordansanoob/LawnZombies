3ee00bbf1adcf464fe404b71a264b9773be52f7d
allierowe4 <acr2785@uncw.edu>
2016-04-25 23:39:02 -0400

    Update EntityB.java

--- EntityB.java
+++ EntityB.java
@@ -1,6 +1,11 @@
+/** @author Allison Rowe, Jack Erwin, Jordan A.
+ * (Allison Created this)
+ */ 
 import java.awt.Graphics;
 import java.awt.Rectangle;
-
+/**
+ *EntityB interface
+ */ 
 public interface EntityB {
 
 	public void tick();
